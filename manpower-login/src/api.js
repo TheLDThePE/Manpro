6d22d66b168ea9fafcb436294b8c26d8e46899b5
Theldthepe <Theldthepe@gmail.com>
2024-11-16 16:04:31 +0700

    Create api.js

---
+++ manpower-login/src/api.js
@@ -0,0 +1,11 @@
+export async function login(username, password) {
+    const response = await fetch('https://api.example.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    return response.json();
+  }
+  
